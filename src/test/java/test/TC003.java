bf0643f920ab158032911a8fcca038e3c0f28a1b
akshay bhavare <akshaybhavare03@gmail.com>
2023-03-17 11:45:51 +0530

    t to--> T

--- src/test/java/test/TC003.java
+++ src/test/java/test/TC003.java
@@ -4,7 +4,10 @@ public class TC003 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test case 3");
+		System.out.println("Test case 3");
+		
+		
+		
 	}
 
 }
